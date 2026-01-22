ba4cb1842797104ef5e213698c71a1608f67a9ea
Henil2007 <henilapatel2000007@gmail.com>
2026-01-22 20:09:25 +0530

    Practice

--- Practice/Test.py
+++ Practice/Test.py
@@ -1,7 +1,53 @@
-name = input("ENter name : ")
+# a = int(input("Enter number : "))
+# b = int(input("Enter number : "))
 
-lower = name.lower()
-upper = name.upper()
+# c = a >= b
+# d = a < b
+# print(c)
+# print(d)
 
-print(lower)
-print(upper)
+
+# name = input("Enter name : ")
+# dol = 0
+
+# for i in name:
+#     if(i == '$'):
+#         dol += 1
+# print(dol)
+
+
+# num = int(input("Enter a number : "))
+# if(num % 10 == 0):
+#     print("Even number.")
+# else:
+#     print("Odd number.")
+
+
+# a = int(input("Enter a number : "))
+# b = int(input("Enter a number : "))
+# c = int(input("Enter a number : "))
+
+# if(a>b):
+#     if(a>c):
+#         print(a)
+#     else:
+#         print(c)
+# else:
+#     if(b>c):
+#         print(b)
+#     else:
+#         print(c)
+
+# num = int(input("Enter number : "))
+# sum = 0
+# i = 1
+# while i <= num:
+#     sum = sum + i
+#     i += 1
+# print(sum)
+
+num = int(input("ENter number : "))
+fac = 1
+for i in range(num,0,-1):
+    fac *= i
+print(fac)
